b9065a24caaa97377f4a2fd44bce795d5b834424
Veaceslav Barbarii <slava.barbarii@gmail.com>
2023-06-08 10:46:40 +0300

    Tried out to combine both configuration in a single file.

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+// don't use it
+// it's just an another way to create webpack config
+// but in a single file
+
+const path = require('path');
+const { merge } = require('webpack-merge');
+
+const baseConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(css|sass|scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            }
+        ],
+    },
+};
+
+const frontendConfig = merge(baseConfig, {
+        entry: './src/index.jsx',
+        output: {
+            path: path.resolve(__dirname, 'dist/frontend'),
+            filename: 'main.js',
+            libraryTarget: 'umd',
+        },
+        name: 'frontend',
+    })
+
+const serverConfig = merge(baseConfig, {
+    entry: './src/server.js',
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, 'dist/server'),
+        filename: 'main.js',
+        libraryTarget: 'commonjs'
+    },
+    name: 'server',
+});
+
+module.exports = [
+    frontendConfig,
+    serverConfig
+];
