ae9aab02f82dfa68fdda538edd5fe38600cdf227
Veaceslav Barbarii <slava.barbarii@gmail.com>
2023-06-10 21:11:45 +0300

    Added webpack dev server HMR

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,3 +1,5 @@
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.base.config.js');
 
@@ -6,8 +8,30 @@ const config = {
   devServer: {
     static: {
       directory: './public',
-    }
-  }
-}
+    },
+    // hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              plugins: [
+                require.resolve('react-refresh/babel'),
+              ],
+            }
+          }
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new ReactRefreshWebpackPlugin()
+  ],
+};
 
 module.exports = merge(baseConfig, config);
